691bd63de47641f0c043db6d399e7a664e928a71
MurphyLee <36724748+MurphyLee2048@users.noreply.github.com>
2019-07-24 00:26:18 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList'
+
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    ToDoList
+  },
+  mounted() {
+    this.$store.dispatch('getList');
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./assets/App.css">
+
 </style>
